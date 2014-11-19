e791713046a53dcafdb603f3fd09e2d1c93b4579
Ubu <bierbram@tf.uni-freiburg.de>
2014-11-19 09:17:07 +0100

    Added HelloNexus Projekt

---
+++ src/com/OrcaOrange/hellonexus5/MainActivity.java
@@ -0,0 +1,53 @@
+package com.OrcaOrange.hellonexus5;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+	
+TextView txt;
+RatingBar rate;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.textView1);
+        rate = (RatingBar) findViewById(R.id.ratingBar1);
+        rate.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+			
+			@Override
+			public void onRatingChanged(RatingBar ratingBar, float rating,
+					boolean fromUser) {
+				// TODO Auto-generated method stub
+				txt.setText("" + rating);
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
